025619e5d56d4d1f6dbc574260ec03845cb26271
almogun9963 <47495987+almogun9963@users.noreply.github.com>
2020-06-18 14:34:18 +0300

    Add files via upload

---
+++ filterfalse.hpp
@@ -0,0 +1,93 @@
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+#include <iterator>
+
+using namespace std;
+
+namespace itertools
+{
+	template <typename Function,typename Container>
+	class filterfalse
+    {
+		private:
+            const Function& f;
+			const Container& container;
+			
+		public:
+			filterfalse(const Function& f,const Container& cont ):container(cont),f(f)
+			{
+
+			}
+
+		class iterator
+        {
+			private:
+				decltype(container.begin()) beg;
+				decltype(container.end()) end;
+				Function f;
+
+			public:
+				iterator(decltype(container.begin()) p,decltype(container.end()) end,Function f):
+					beg(p),end(end),f(f)
+					{
+
+					}
+
+				++i;
+				iterator& operator++()
+				{
+                    ++beg;
+                    while(f(*beg) && beg!=end)
+                    {
+                        ++beg;
+                    }
+					return *this;
+				}
+				iterator operator++(int)
+				{
+					iterator tmp= *this;
+                    beg++;
+                    while(f(*beg) && beg!=end)
+                    {
+                        beg++;
+                    }
+					return tmp;
+				}
+
+                auto operator*()
+                {
+                    if(f(*beg)&& beg!=end)
+                    {
+                        (*this)++;
+                    }
+                    return *beg;
+				}
+
+				bool operator==(const iterator& i) const
+				{
+					return beg == i.beg;
+				}
+
+				bool operator!=(const iterator& rhs) const
+				{
+					return beg != i.beg;
+				}
+			};
+
+			iterator begin()const
+			{
+				return iterator(container.begin(),container.end(),f);
+			}
+
+			iterator end() const
+			{
+				return iterator(container.end(),container.end(),f);
+			}
+	};
+}
+
+
---
+++ range.hpp
@@ -0,0 +1,72 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace itertools
+{
+	class range
+    {
+		public:
+			int start;
+			int finish;
+
+			range(int x,int y):start(x),finish(y)
+			{
+				if(x>y)
+				{
+					throw new std::logic_error("there is no range\n");
+				}
+			}
+
+	class iterator
+    {
+		private:
+			int current;
+		public:
+
+			iterator(int num)
+			    : current(num)
+				{
+
+				}
+
+			int operator*() const
+			{
+				return current;
+			}
+			iterator& operator++()
+			{
+				(current)++;
+				return *this;
+			}
+			const iterator operator++(int)
+			{
+				iterator temp = *this;
+                current++;
+				return temp;
+			}
+
+			bool operator==(const iterator& i) const
+			{
+				return current == i.current;
+			}
+
+			bool operator!=(const iterator& i) const
+			{
+				return current != i.current;
+			}
+		};
+
+		iterator begin() const
+		{
+			return iterator{start};
+		}
+		
+		iterator end() const
+		{
+			return iterator{finish};
+		}
+	};
+}
